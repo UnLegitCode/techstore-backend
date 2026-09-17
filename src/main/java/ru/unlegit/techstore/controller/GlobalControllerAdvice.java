0c1de0423e0fe9838f382c6f0f211fc12340a493
UnLegit <ZubMax2005@yandex.ru>
2026-09-17 15:10:18 +0300

    feat: configure Page serialization & implement missing exception handlers in controller advice

--- src/main/java/ru/unlegit/techstore/controller/GlobalControllerAdvice.java
+++ src/main/java/ru/unlegit/techstore/controller/GlobalControllerAdvice.java
@@ -5,28 +5,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.unlegit.techstore.dto.info.ErrorResponse;
-import ru.unlegit.techstore.exception.EmailAlreadyExistsException;
-import ru.unlegit.techstore.exception.InvalidCredentialsException;
-import ru.unlegit.techstore.exception.InvalidOrExpiredTokenException;
+import ru.unlegit.techstore.exception.cart.CartItemNotFoundException;
+import ru.unlegit.techstore.exception.category.ProductCategoryAlreadyExistsException;
+import ru.unlegit.techstore.exception.category.ProductCategoryInUseException;
+import ru.unlegit.techstore.exception.category.ProductCategoryNotFoundException;
+import ru.unlegit.techstore.exception.product.ProductNotFoundException;
+import ru.unlegit.techstore.exception.user.EmailAlreadyExistsException;
+import ru.unlegit.techstore.exception.user.InvalidCredentialsException;
+import ru.unlegit.techstore.exception.user.InvalidOrExpiredTokenException;
 
 @RestControllerAdvice
+@SuppressWarnings("unused")
 public class GlobalControllerAdvice {
 
     @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ErrorResponse handleEmailExists(EmailAlreadyExistsException exception) {
+    @ExceptionHandler({
+            EmailAlreadyExistsException.class,
+            ProductCategoryAlreadyExistsException.class,
+            ProductCategoryInUseException.class
+    })
+    public ErrorResponse handleConflict(Exception exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ErrorResponse handleInvalidCredentials(InvalidCredentialsException exception) {
+    @ExceptionHandler({InvalidCredentialsException.class, InvalidOrExpiredTokenException.class})
+    public ErrorResponse handleUnauthorized(Exception exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(InvalidOrExpiredTokenException.class)
-    public ErrorResponse handleInvalidOrExpiredToken(InvalidOrExpiredTokenException exception) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({
+            ProductCategoryNotFoundException.class,
+            ProductNotFoundException.class,
+            CartItemNotFoundException.class
+    })
+    public ErrorResponse handleNotFound(Exception exception) {
         return new ErrorResponse(exception.getMessage());
     }
 }
